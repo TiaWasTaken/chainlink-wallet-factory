e48428c37afa763d73df35407db39eff9983f904
Mattia Arganetto <arganetto.mattia@gmail.com>
2025-11-11 11:34:19 +0100

    refined the hero

--- frontend/src/components/Hero.jsx
+++ frontend/src/components/Hero.jsx
@@ -29,7 +29,7 @@ const Hero = () => {
           <p
             className={`${styles.heroSubText} mt-2 text-white-100 text-[16px]`}
           >
-            Powered by <span className="text-[#915eff]">Ethereum </span> 
+            Powered by <span className="text-[#915eff]">Ethereum </span>
             <br />Built for{" "}
             <span className="text-[#915eff]">Connection</span>
           </p>
@@ -68,23 +68,6 @@ const Hero = () => {
       {/* === 3D model === */}
       <div className="relative z-0 mt-[-40px] mb-[-40px]">
         <ComputersCanvas />
-      </div>
-
-      {/* === Scroll indicator === */}
-      <div className="absolute xs:bottom-10 bottom-28 w-full flex justify-center items-center z-10">
-        <a href="#about" className="relative">
-          <div className="w-[35px] h-[64px] rounded-3xl border-4 border-[#915eff] hover:border-white flex justify-center items-center cursor-pointer">
-            <motion.div
-              animate={{ y: [-10, 15, -10] }}
-              transition={{
-                duration: 1.5,
-                repeat: Infinity,
-                repeatType: "loop",
-              }}
-              className="w-3 h-3 rounded-full bg-[#915eff] mb-1"
-            />
-          </div>
-        </a>
       </div>
     </section>
   );
