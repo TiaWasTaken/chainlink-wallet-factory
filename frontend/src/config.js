8cd92fb3388b9d2cfbaff0ee65d594d25e23e1df
Mattia Arganetto <arganetto.mattia@gmail.com>
2025-12-05 11:21:58 +0100

    CHECKPOINT

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,9 +1,10 @@
 // src/config.js
-export const WALLET_FACTORY_ADDRESS = "0x9fE46736679d2D9a65F0992F2272dE9f3c7fa6e0";
+import addresses from "./abi/addresses.json";
+
+export const WALLET_FACTORY_ADDRESS = addresses.WalletFactory;
+export const ORACLE_PRICE_ADDRESS = addresses.OraclePrice;
 
 export const ETHERSCAN_API_KEY = import.meta.env.VITE_ETHERSCAN_API_KEY;
 export const ETHERSCAN_API_BASE = "https://api-sepolia.etherscan.io";
 export const ETHERSCAN_EXPLORER_BASE = "https://sepolia.etherscan.io";
-
-export const ORACLE_PRICE_ADDRESS = "0x5FC8d32690cc91D4c39d9d3abcBD16989F875707";
 
